b2f7c095718a08c00f1cd8b8168c47b9694753c0
MCE626 <Contact.MCE626@gmail.com>
2018-09-25 10:21:48 -0500

    Updated logo

--- main/java/lu/com/mce/util/References.java
+++ main/java/lu/com/mce/util/References.java
@@ -3,7 +3,7 @@ package lu.com.mce.util;
 public class References {
 	public static final String MOD_ID = "mod_lu";
 	public static final String NAME = "LittleUtilities";
-	public static final String VERSION = "1.12.2-b1.0";
+	public static final String VERSION = "1.12.2-1.0";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 	
 	public static final String CLIENT_PROXY_CLASS = "lu.com.mce.proxy.ClientProxy";
