858c29c9b176e7d41de83ce9535d255a63abbd04
MCE626 <Contact.MCE626@gmail.com>
2018-10-20 17:32:07 -0500

    Pure quartz is disabled is there isn't a mod installed with silicon in the ore dictionary.

--- main/java/mce/lu/common/util/config/conditions/PureQuartzRecipe.java
+++ main/java/mce/lu/common/util/config/conditions/PureQuartzRecipe.java
@@ -1,9 +1,11 @@
 package mce.lu.common.util.config.conditions;
 
 import java.util.function.BooleanSupplier;
+import java.util.logging.Level;
 
 import com.google.gson.JsonObject;
 
+import mce.lu.common.LittleUtilities;
 import mce.lu.common.util.config.LUConfigManager;
 import net.minecraftforge.common.crafting.IConditionFactory;
 import net.minecraftforge.common.crafting.JsonContext;
@@ -11,7 +13,14 @@ import net.minecraftforge.oredict.OreDictionary;
 
 public class PureQuartzRecipe implements IConditionFactory {
 	public BooleanSupplier parse(JsonContext context, JsonObject json) {
-		return () -> OreDictionary.doesOreNameExist("ingotSilicon") ? OreDictionary.doesOreNameExist("itemSilicon")
-				: LUConfigManager.pureQuartzRecipe;
+		if (OreDictionary.doesOreNameExist("ingotSilicon") || OreDictionary.doesOreNameExist("itemSilicon")) {
+			LittleUtilities.logger.log(Level.INFO,
+					"Found a mod with silicon in the ore dictionary. Enabling pure quartz.");
+			return () -> LUConfigManager.pureQuartzRecipe;
+		} else {
+			LittleUtilities.logger.log(Level.INFO,
+					"Did not find a mod with silicon in the ore dictionary. Disabling pure quartz.");
+			return () -> false;
+		}
 	}
 }
