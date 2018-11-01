e92079cb70b77da8e8d9d05c5e3b055ce3b7d0d0
MCE626 <Contact.MCE626@gmail.com>
2018-11-01 18:00:04 -0500

    Fixed a bug where the condenser didn't drop all items in it's inventory is it was broken/mined

--- src/main/java/mce/lu/common/block/Condenser.java
+++ src/main/java/mce/lu/common/block/Condenser.java
@@ -8,17 +8,13 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
-import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.InventoryHelper;
-import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraftforge.items.CapabilityItemHandler;
-import net.minecraftforge.items.IItemHandler;
 
 public class Condenser extends BlockContainerBase {
 	public Condenser(String name, Material mat) {
@@ -43,13 +39,11 @@ public class Condenser extends BlockContainerBase {
 
 	@Override
 	public void breakBlock(World world, BlockPos pos, IBlockState state) {
-		TileEntity te = world.getTileEntity(pos);
-		IItemHandler itemHandler = te.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
+		TileEntity tile = world.getTileEntity(pos);
 
-		for (int slot = 0; slot < itemHandler.getSlots() - 1; slot++) {
-			ItemStack stack = itemHandler.getStackInSlot(slot);
-			InventoryHelper.spawnItemStack(world, pos.getX(), pos.getY(), pos.getZ(), stack);
-			// world.updateComparatorOutputLevel(pos, this);
+		if (tile instanceof TileEntityCondenser) {
+			InventoryHelper.dropInventoryItems(world, pos, (TileEntityCondenser) tile);
+			world.updateComparatorOutputLevel(pos, this);
 		}
 
 		super.breakBlock(world, pos, state);
@@ -67,6 +61,6 @@ public class Condenser extends BlockContainerBase {
 
 	@Override
 	public int getComparatorInputOverride(IBlockState blockState, World world, BlockPos pos) {
-		return Container.calcRedstoneFromInventory((IInventory) world.getTileEntity(pos));
+		return Container.calcRedstone(world.getTileEntity(pos));
 	}
 }
