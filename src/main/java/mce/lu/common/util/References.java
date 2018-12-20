a41fa3155c4d6aabdeb8b93d19e16e31651bd9bd
MCE626 <Contact.MCE626@gmail.com>
2018-12-20 00:57:55 -0600

    bug fix for server

--- src/main/java/mce/lu/common/util/References.java
+++ src/main/java/mce/lu/common/util/References.java
@@ -3,9 +3,9 @@ package mce.lu.common.util;
 public class References {
 	public static final String MOD_ID = "littleutilities";
 	public static final String NAME = "LittleUtilities";
-	public static final String VERSION = "1.12.2-beta1.0.0.0";
+	public static final String VERSION = "1.12.2-beta1.0.0.1";
 	public static final String UPDATE = "https://raw.githubusercontent.com/MCE626/LittleUtilities/master/update.json";
 	
 	public static final String CLIENT_PROXY_CLASS = "mce.lu.client.core.proxy.ClientProxy";
-	public static final String SERVER_PROXY_CLASS = "mce.lu.common.core.proxy.ServerProxy";
+	public static final String SERVER_PROXY_CLASS = "mce.lu.common.core.proxy.CommonProxy";
 }
