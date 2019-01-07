179c012935733291395de79d36f4587e4582fa1e
MCE626 <Contact.MCE626@gmail.com>
2019-01-06 23:45:03 -0600

    Fixed some crafting issues.

--- src/main/java/mce/lu/common/util/References.java
+++ src/main/java/mce/lu/common/util/References.java
@@ -3,7 +3,7 @@ package mce.lu.common.util;
 public class References {
 	public static final String MOD_ID = "littleutilities";
 	public static final String NAME = "LittleUtilities";
-	public static final String VERSION = "1.12.2-beta1.0.0.1";
+	public static final String VERSION = "1.12.2-1.0.0.0";
 	public static final String UPDATE = "https://raw.githubusercontent.com/MCE626/LittleUtilities/master/update.json";
 	
 	public static final String CLIENT_PROXY_CLASS = "mce.lu.client.core.proxy.ClientProxy";
