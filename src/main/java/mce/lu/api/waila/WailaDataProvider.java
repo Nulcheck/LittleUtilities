e9eab06a418fa2fc06c18d27fe0873932dd05f78
MCE626 <Contact.MCE626@gmail.com>
2019-02-04 22:39:00 -0600

    Finished waila integration with other stuff

--- src/main/java/mce/lu/api/waila/WailaDataProvider.java
+++ src/main/java/mce/lu/api/waila/WailaDataProvider.java
@@ -1,7 +1,11 @@
 package mce.lu.api.waila;
 
 import java.util.List;
+import java.util.Random;
 
+import mce.lu.common.block.EdibleBlock;
+import mce.lu.common.block.ModBlocks;
+import mce.lu.common.block.UsableBlock;
 import mce.lu.common.entity.tile.TileEntitySnowMelter;
 import mce.lu.common.util.References;
 import mcp.mobius.waila.api.IWailaConfigHandler;
@@ -30,15 +34,18 @@ public class WailaDataProvider implements IWailaDataProvider {
 
 	@Override
 	@Method(modid = References.WAILA)
-	public List<String> getWailaHead(ItemStack stack, List<String> tip, IWailaDataAccessor data,
+	public List<String> getWailaHead(ItemStack stack, List<String> list, IWailaDataAccessor data,
 			IWailaConfigHandler config) {
-		return tip;
+		return list;
 	}
 
 	@Override
 	@Method(modid = References.WAILA)
-	public List<String> getWailaBody(ItemStack stack, List<String> tip, IWailaDataAccessor data,
+	public List<String> getWailaBody(ItemStack stack, List<String> list, IWailaDataAccessor data,
 			IWailaConfigHandler config) {
+		Random rand = new Random();
+		int i = rand.nextInt(150);
+
 		Block block = data.getBlock();
 		TileEntity tile = data.getTileEntity();
 		NBTTagCompound tag = data.getNBTData();
@@ -46,19 +53,53 @@ public class WailaDataProvider implements IWailaDataProvider {
 		int snowMelterRange = data.getNBTInteger(data.getNBTData(), "SnowMelterRange");
 		boolean snowMelterRedstoneMode = tag.getBoolean("SnowMelterRedstoneMode");
 
+		// Snow Melter
 		if (tile instanceof TileEntitySnowMelter) {
-			tip.add("Range: " + snowMelterRange);
-			tip.add("Redstone Mode: " + snowMelterRedstoneMode);
+			list.add("Range: " + snowMelterRange);
+			list.add("Redstone Mode: " + snowMelterRedstoneMode);
 		}
 
-		return tip;
+		// Normal Blocks
+		if (block == ModBlocks.BLAZE_ROD_BLOCK || block == ModBlocks.BLAZE_POWDER_BLOCK
+				|| block == ModBlocks.BLAZE_BARS)
+			list.add("Hot!");
+
+		if (block == ModBlocks.PUFFERFISH_BLOCK)
+			list.add("Poisonous!");
+
+		// Edible and Usable Blocks
+		if (block instanceof EdibleBlock) {
+			int meta = 9 - data.getMetadata();
+
+			if (meta > 1)
+				list.add(meta + " Bites Left");
+
+			else if (meta == 1) {
+				if (i == 23)
+					list.add(meta + " Byte Left");
+				else
+					list.add(meta + " Bite Left");
+			}
+		}
+
+		if (block instanceof UsableBlock) {
+			int meta = 9 - data.getMetadata();
+
+			if (meta > 1)
+				list.add(meta + " Uses Left");
+
+			else if (meta == 1)
+				list.add(meta + " Use Left");
+		}
+
+		return list;
 	}
 
 	@Override
 	@Method(modid = References.WAILA)
-	public List<String> getWailaTail(ItemStack stack, List<String> tip, IWailaDataAccessor data,
+	public List<String> getWailaTail(ItemStack stack, List<String> list, IWailaDataAccessor data,
 			IWailaConfigHandler config) {
-		return tip;
+		return list;
 	}
 
 	@Override
